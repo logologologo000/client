64103278d4f711749c51efab068bea0b15697566
ALOGOM <60058140+logologologo000@users.noreply.github.com>
2021-07-15 01:46:54 +0700

    Admin overflow

--- src/components/Admin.js
+++ src/components/Admin.js
@@ -4,14 +4,15 @@ import './css/Admin.css'
 function admin() {
     return (
         <div>
+
             <div className="container-fluid mt-4">
                 <div className="row">
                     {
                         //Status
                     }
-                    <div className="col col-sm-5 col-12 mb-4" >
+                    <div className=" col col-sm-5 col-12 mb-4" >
 
-                        <div className="card  widthcard p-3">
+                        <div className="area card  widthcard p-3">
                             <h5 className="card-title xcenter wordupper">
                                 Status
                             </h5>
@@ -20,10 +21,40 @@ function admin() {
                                     <h6 className=" text-muted wordlower ">
                                         request
                                     </h6>
+                                    <h6 className=" text-muted wordlower ">
+                                        fixing
+                                    </h6>
+                                    <h6 className=" text-muted wordlower ">
+                                        fixed
+                                    </h6>
+                                    <h6 className=" text-muted wordlower ">
+                                        class
+                                    </h6>
+                                    <h6 className=" text-muted wordlower ">
+                                        TA
+                                    </h6>
+                                    <h6 className=" text-muted wordlower ">
+                                        total request
+                                    </h6>
                                 </div>
                                 <div className="col-6">
                                     <h6 className=" text-muted wordlower ">
-                                        56 unit
+                                        70 unit
+                                    </h6>
+                                    <h6 className=" text-muted wordlower ">
+                                        3 unit
+                                    </h6>
+                                    <h6 className=" text-muted wordlower ">
+                                        7 unit
+                                    </h6>
+                                    <h6 className=" text-muted wordlower ">
+                                        5 unit
+                                    </h6>
+                                    <h6 className=" text-muted wordlower ">
+                                        4 unit
+                                    </h6>
+                                    <h6 className=" text-muted wordlower ">
+                                        80 unit
                                     </h6>
 
                                 </div>
@@ -38,33 +69,268 @@ function admin() {
                     {
                         //List
                     }
+
                     <div className="col col-12 col-sm-7 ">
-                        <div className="card  widthcard " id="contents">
-                            <div className="card-body">
-                                <h5 className="card-title">404 Not Found</h5>
-                                <h6 className="card-subtitle mb-2 text-muted">Pannawit</h6>
-                                <p className="card-text">
-                                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Sit quod
-                                    ullam aliquam magnam! Exercitationem veritatis ab, sequi harum
-                                    blanditiis rem!
-                                </p>
+                        <div className="">
+                            {
+                                // rows
+                            }
+                            <div className="area card widthcard list-scroll " >
 
-                                <button
+                                {
+                                    //loop here
+                                }
 
-                                    className="btn btn-success btn-sm mr-3 buttontext"
+                                <div id="contents">
+                                    <div className="card-body">
+                                        <div>
+                                            <h5 className="card-title d-inline">404 Not Found</h5>
+                                            <button
 
-                                >
-                                    view
-                                </button>
-                                <button
-                                    className="btn btn-success btn-sm  my-1 buttontext"
+                                                className="d-inline-f-r btn btn-outline-success btn-sm  buttontext"
 
-                                >
-                                    des
-                                </button>
+                                            >
+                                                fix
+                                            </button>
+                                            <button
+                                                className=" d-inline-f-r btn btn-outline-success btn-sm   buttontext"
+                                                data-bs-toggle="collapse" data-bs-target="#idcol1"
+                                            >
+                                                view
+                                            </button>
+                                        </div>
+
+                                        <h6 className="card-subtitle my-1 text-muted">Pannawit</h6>
+
+                                        <div class="collapse" id="idcol1">
+                                            <div class="py-1">
+
+                                                <p >
+                                                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Sit quod
+                                                    ullam aliquam magnam! Exercitationem veritatis ab, sequi harum
+                                                    blanditiis rem!
+                                                </p>
+                                            </div>
+                                        </div>
+                                        <hr />
+                                    </div>
+                                </div>
+
+                                <div id="contents">
+                                    <div className="card-body">
+                                        <div>
+                                            <h5 className="card-title d-inline">404 Not Found</h5>
+                                            <button
+
+                                                className="d-inline-f-r btn btn-outline-success btn-sm  buttontext"
+
+                                            >
+                                                fix
+                                            </button>
+                                            <button
+                                                className=" d-inline-f-r btn btn-outline-success btn-sm   buttontext"
+                                                data-bs-toggle="collapse" data-bs-target="#idcol2"
+                                            >
+                                                view
+                                            </button>
+                                        </div>
+
+                                        <h6 className="card-subtitle my-1 text-muted">Pannawit</h6>
+
+                                        <div class="collapse" id="idcol2">
+                                            <div class="py-1">
+
+                                                <p >
+                                                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Sit quod
+                                                    ullam aliquam magnam! Exercitationem veritatis ab, sequi harum
+                                                    blanditiis rem!
+                                                </p>
+                                            </div>
+                                        </div>
+                                        <hr />
+                                    </div>
+                                </div>
+
+                                <div id="contents">
+                                    <div className="card-body">
+                                        <div>
+                                            <h5 className="card-title d-inline">404 Not Found</h5>
+                                            <button
+
+                                                className="d-inline-f-r btn btn-outline-success btn-sm  buttontext"
+
+                                            >
+                                                fix
+                                            </button>
+                                            <button
+                                                className=" d-inline-f-r btn btn-outline-success btn-sm   buttontext"
+                                                data-bs-toggle="collapse" data-bs-target="#idcol3"
+                                            >
+                                                view
+                                            </button>
+                                        </div>
+
+                                        <h6 className="card-subtitle my-1 text-muted">Pannawit</h6>
+
+                                        <div class="collapse" id="idcol3">
+                                            <div class="py-1">
+
+                                                <p >
+                                                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Sit quod
+                                                    ullam aliquam magnam! Exercitationem veritatis ab, sequi harum
+                                                    blanditiis rem!
+                                                </p>
+                                            </div>
+                                        </div>
+                                        <hr />
+                                    </div>
+                                </div>
+
+                                <div id="contents">
+                                    <div className="card-body">
+                                        <div>
+                                            <h5 className="card-title d-inline">404 Not Found</h5>
+                                            <button
+
+                                                className="d-inline-f-r btn btn-outline-success btn-sm  buttontext"
+
+                                            >
+                                                fix
+                                            </button>
+                                            <button
+                                                className=" d-inline-f-r btn btn-outline-success btn-sm   buttontext"
+                                                data-bs-toggle="collapse" data-bs-target="#idcol4"
+                                            >
+                                                view
+                                            </button>
+                                        </div>
+
+                                        <h6 className="card-subtitle my-1 text-muted">Pannawit</h6>
+
+                                        <div class="collapse" id="idcol4">
+                                            <div class="py-1">
+
+                                                <p >
+                                                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Sit quod
+                                                    ullam aliquam magnam! Exercitationem veritatis ab, sequi harum
+                                                    blanditiis rem!
+                                                </p>
+                                            </div>
+                                        </div>
+                                        <hr />
+                                    </div>
+                                </div>
+
+                                <div id="contents">
+                                    <div className="card-body">
+                                        <div>
+                                            <h5 className="card-title d-inline">404 Not Found</h5>
+                                            <button
+
+                                                className="d-inline-f-r btn btn-outline-success btn-sm  buttontext"
+
+                                            >
+                                                fix
+                                            </button>
+                                            <button
+                                                className=" d-inline-f-r btn btn-outline-success btn-sm   buttontext"
+                                                data-bs-toggle="collapse" data-bs-target="#idcol5"
+                                            >
+                                                view
+                                            </button>
+                                        </div>
+
+                                        <h6 className="card-subtitle my-1 text-muted">Pannawit</h6>
+
+                                        <div class="collapse" id="idcol5">
+                                            <div class="py-1">
+
+                                                <p >
+                                                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Sit quod
+                                                    ullam aliquam magnam! Exercitationem veritatis ab, sequi harum
+                                                    blanditiis rem!
+                                                </p>
+                                            </div>
+                                        </div>
+                                        <hr />
+                                    </div>
+                                </div>
+
+                                <div id="contents">
+                                    <div className="card-body">
+                                        <div>
+                                            <h5 className="card-title d-inline">404 Not Found</h5>
+                                            <button
+
+                                                className="d-inline-f-r btn btn-outline-success btn-sm  buttontext"
+
+                                            >
+                                                fix
+                                            </button>
+                                            <button
+                                                className=" d-inline-f-r btn btn-outline-success btn-sm   buttontext"
+                                                data-bs-toggle="collapse" data-bs-target="#idcol6"
+                                            >
+                                                view
+                                            </button>
+                                        </div>
+
+                                        <h6 className="card-subtitle my-1 text-muted">Pannawit</h6>
+
+                                        <div class="collapse" id="idcol6">
+                                            <div class="py-1">
+
+                                                <p >
+                                                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Sit quod
+                                                    ullam aliquam magnam! Exercitationem veritatis ab, sequi harum
+                                                    blanditiis rem!
+                                                </p>
+                                            </div>
+                                        </div>
+                                        <hr />
+                                    </div>
+                                </div>
+
+                                <div id="contents">
+                                    <div className="card-body">
+                                        <div>
+                                            <h5 className="card-title d-inline">404 Not Found</h5>
+                                            <button
+
+                                                className="d-inline-f-r btn btn-outline-success btn-sm  buttontext"
+
+                                            >
+                                                fix
+                                            </button>
+                                            <button
+                                                className=" d-inline-f-r btn btn-outline-success btn-sm   buttontext"
+                                                data-bs-toggle="collapse" data-bs-target="#idcol7"
+                                            >
+                                                view
+                                            </button>
+                                        </div>
+
+                                        <h6 className="card-subtitle my-1 text-muted">Pannawit</h6>
+
+                                        <div class="collapse" id="idcol7">
+                                            <div class="py-1">
+
+                                                <p >
+                                                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Sit quod
+                                                    ullam aliquam magnam! Exercitationem veritatis ab, sequi harum
+                                                    blanditiis rem!
+                                                </p>
+                                            </div>
+                                        </div>
+                                        <hr />
+                                    </div>
+                                </div>
+
 
                             </div>
                         </div>
+
+
                     </div>
 
                 </div>
--- src/components/App.js
+++ src/components/App.js
@@ -10,9 +10,9 @@ function App() {
   return (
 
           <div>
-              <Navbar />
-
               <Admin />
+
+              
           </div>
 
   );
