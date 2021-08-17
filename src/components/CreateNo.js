fdcf2233ae945f034ab87583426f507ac038f1a2
ALOGOM <60058140+logologologo000@users.noreply.github.com>
2021-08-17 20:26:04 +0700

    create note 40%

--- src/components/CreateNo.js
+++ src/components/CreateNo.js
@@ -4,7 +4,33 @@ const CreateNo = () => {
 
     return (
         <div className="container">
-            <div className="formmmm my-3">
+            <div className="formmmm my-3 text-center p-4 col-10  col-md-10 mx-auto">
+                <h4 className="color-yellow lt-sp">CREATE NOTICE</h4>
+                <div className="border-content mt-3 p-3">
+                    <div className="p-3">
+                        <label className="f-l">title</label>
+                        <input type="text" className="f-r  col-md-8"/>
+                        <br />
+                        <hr class="mt-4 mb-0" />
+                    </div>
+                    
+                    <div className="p-3">
+                        <label className="f-l">title</label>
+                        <input type="text" className="f-r  col-md-8"/>
+                        <br />
+                        <hr class="mt-4 mb-0" />
+                    </div>
+
+                    <div className="p-3">
+                        <label className="f-l">title</label>
+                        <input type="text" className="f-r  col-md-8"/>
+                        <br />
+                        <hr class="" />
+                    </div>
+                   
+                    
+                   
+                </div>
 
             </div>
         </div>
